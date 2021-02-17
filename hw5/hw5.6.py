2c3e7e4afdcb79e8f6138b7e16bde209d36bc61a
Ivan <iv.avde@gmail.com>
2021-02-17 10:02:17 +0100

    homework 4, 5

---
+++ hw5/hw5.6.py
@@ -0,0 +1,7 @@
+rasp = {}
+with open("hw5.6.txt", encoding="utf-8") as f:
+    for line in f:
+        name, stats = line.split(':')
+        name_sum = sum(map(int, "".join([i for i in stats if i == ' ' or (i >= '0' and i <= '9')]).split()))
+        rasp[name] = name_sum
+print(f"{rasp}")
